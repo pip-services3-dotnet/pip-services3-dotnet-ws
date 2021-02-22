8d0e3c5b8d2aab8e1e93d04e97558a8e90b4376e
Sergey Seroukhov <seroukhov@gmail.com>
2021-02-22 13:59:36 -0700

    Added checkout script

---
+++ checkout.ps1
@@ -0,0 +1,24 @@
+#!/usr/bin/env pwsh
+
+git clone git@github.com:pip-services3-dotnet/pip-services3-commons-dotnet
+git clone git@github.com:pip-services3-dotnet/pip-services3-components-dotnet
+git clone git@github.com:pip-services3-dotnet/pip-services3-expressions-dotnet
+git clone git@github.com:pip-services3-dotnet/pip-services3-container-dotnet
+git clone git@github.com:pip-services3-dotnet/pip-services3-data-dotnet
+git clone git@github.com:pip-services3-dotnet/pip-services3-rpc-dotnet
+git clone git@github.com:pip-services3-dotnet/pip-services3-grpc-dotnet
+git clone git@github.com:pip-services3-dotnet/pip-services3-swagger-dotnet
+git clone git@github.com:pip-services3-dotnet/pip-services3-messaging-dotnet
+git clone git@github.com:pip-services3-dotnet/pip-services3-mongodb-dotnet
+git clone git@github.com:pip-services3-dotnet/pip-services3-postgres-dotnet
+git clone git@github.com:pip-services3-dotnet/pip-services3-sqlserver-dotnet
+git clone git@github.com:pip-services3-dotnet/pip-services3-sqlite-dotnet
+git clone git@github.com:pip-services3-dotnet/pip-services3-mysql-dotnet
+git clone git@github.com:pip-services3-dotnet/pip-services3-elasticsearch-dotnet
+git clone git@github.com:pip-services3-dotnet/pip-services3-memcached-dotnet
+git clone git@github.com:pip-services3-dotnet/pip-services3-redis-dotnet
+git clone git@github.com:pip-services3-dotnet/pip-services3-prometheus-dotnet
+git clone git@github.com:pip-services3-dotnet/pip-services3-rabbitmq-dotnet
+git clone git@github.com:pip-services3-dotnet/pip-services3-datadog-dotnet
+git clone git@github.com:pip-services3-dotnet/pip-services3-azure-dotnet
+git clone git@github.com:pip-services3-dotnet/pip-services3-aws-dotnet
--- workspace.build.ps1
+++
@@ -1,4 +0,0 @@
-task Init {
-    Invoke-Task -Task Clone -Path . -Workspace
-    Invoke-Task -Task InstallDep -Path . -All
-}
